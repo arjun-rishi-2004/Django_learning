21453fa3ca1f507e9159b22ecff273216fc0ecbc
Arjun Rishi <128972815+arjun-rishi-2004@users.noreply.github.com>
2023-03-28 14:46:30 +0000

    First Succesful Commit

---
+++ blog_project/blog_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+
+
+    def __str__(self):
+        return f'{self.title}'
+    
+
---
+++ blog_project/blog_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Blog
+
+# Create your views here.
+def home(request):
+    data=Blog.objects.all()
+    return render(request,"home.html",{'blogs':data})
+
+def about(request):
+    return HttpResponse('about us')
+
+
+
+
+def add(request):
+    title=request.POST.get('title')
+    content=request.POST.get('content')
+    if title and content:
+        blog=Blog(title=title,content=content)
+        blog.save()
+        return render(request,'success.html')
+
+    return HttpResponse("Unsuccessful")
+
+def write(request):
+    return render(request,'write.html')
